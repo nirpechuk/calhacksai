869f09ce6813e8cf6ade141dbc54149161cfe8ad
Ron Pechuk <rpechuk@gmail.com>
2025-06-21 18:31:06 -0700

    initial messaging implementation

---
+++ src/utils/messaging.ts
@@ -0,0 +1,18 @@
+import { defineExtensionMessaging, GetDataType } from '@webext-core/messaging';
+import { defineWindowMessaging } from '@webext-core/messaging/page';
+
+
+
+interface ProtocolMap {
+    // TODO: add api schema here
+    activate(): void;
+    highlight(selector: string): void;
+}
+
+// NOTE: this is to send messages between the background script and the content script
+export const extensionMessenger = defineExtensionMessaging<ProtocolMap>();
+
+// NOTE: this is to send messages between the injected script and the content script
+export const websiteMessenger = defineWindowMessaging<ProtocolMap>({
+    namespace: 'website-messenger',
+});
